965b11a40be96b1d5f44a783be931437b9018310
johnsimon21 <120222249+johnsimon21@users.noreply.github.com>
2024-10-11 07:36:06 +0100

    add proposal (seeder, factory and model)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        User::query()->delete();
         User::factory()->count(200)->create();
-
+        
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]));
+        ->each(function (User $u) {
+            
+            $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id'=> $project->id]);
+            });
     }
 }
