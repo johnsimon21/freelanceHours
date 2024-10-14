aa1e41d517d25a2fd64d445dcd22b7eeda704103
johnsimon21 <120222249+johnsimon21@users.noreply.github.com>
2024-10-14 19:16:13 +0100

    Salving proposals

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -18,14 +18,26 @@ class Create extends Component
     #[Rule(["required","numeric", 'gt:0'])]
 
     public int $hours = 0;
+    public bool $agree = false;
 
     public function save()
     {
+        
+        $this->validate();
+        
+        if(!$this->agree){
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+        
+            return;
+        }
+
         $this->project->proposals()
             ->updateOrCreate(
                 ['email' => $this->email],
                 ['hours' => $this->hours]
             );
+
+            $this->modal = false;
     }
     public function render()
     {
